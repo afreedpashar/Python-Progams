676600c251aff4beb9c185d07f40f4f382b7e0d0
afreed <afreedpasha1998@gmail.com>
2024-01-31 19:28:53 +0530

    new programs

--- interview_problems/common_letter.py
+++ interview_problems/common_letter.py
@@ -1,14 +1,29 @@
 from collections import Counter
+# def common(str1,str2):
+#     dict1 = Counter(str1)
+#     dict2 = Counter(str2)
+#     commonDict = dict1 & dict2
+#     if len(commonDict)==0:
+#         print(-1)
+#         return
+#     commonChar = list(commonDict.elements())
+#     commonChar = sorted(commonChar)
+#     print(''.join(commonChar))
+# str1 = "NAINA"
+# str2 = "REENE"
+# print(common(str1,str2))
+
 def common(str1,str2):
-    dict1 = Counter(str1)
-    dict2 = Counter(str2)
+    dict2=Counter(str1)
+    dict1=Counter(str2)
     commonDict = dict1 & dict2
     if len(commonDict)==0:
         print(-1)
         return
     commonChar = list(commonDict.elements())
-    commonChar = sorted(commonChar)
+    commonChar=sorted(commonChar)
     print(''.join(commonChar))
-str1 = "NAINA"
-str2 = "REENE"
+
+str1 = "AFREED"
+str2 = "NAWAZ"
 print(common(str1,str2))
--- interview_problems/target_sum.py
+++ interview_problems/target_sum.py
@@ -1,21 +1,29 @@
-# def target_sum(arr,ts):
-#     a_len=len(arr)
-#     if a_len<2:
-#         print("No such pair")
-#     for i in range(a_len):
-#         for j in range(i+1,a_len):
-#             if arr[i]+arr[j]==ts:
-#                 print(arr[i],arr[j])
+def target_sum(arr,ts):
+    a_len=len(arr)
+    count=0
+    if a_len<2:
+        print("No such pair")
+    for i in range(a_len):
+        for j in range(i+1,a_len):
+            if arr[i]+arr[j]==ts:
+                count+=1
+                print(count)
+                print(arr[i],arr[j])
                 
 
                 
-# ts=7
-# arr=[4,5,6,8,1,2,7,12]
+ts=7
+arr=[4,5,6,8,1,2,7,12]
 # (target_sum(arr,ts))
     
 arr=[4,5,6,8,1,2,7,12]
 ts = 9
+c=0
 for i in range(len(arr)):
     for j in range(i+1,len(arr)):
         if arr[i]+arr[j]==ts:
+            c+=1
+            print("the count is", c)
             print(arr[i],"+",arr[j] ,"=",ts,"pair found at index", i,'and',j)
+
+
