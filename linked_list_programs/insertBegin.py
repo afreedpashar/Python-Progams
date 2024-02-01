5fb6064ab9ce0344ef146ab5a46d0f96fb6f15c9
afreed <afreedpasha1998@gmail.com>
2024-02-01 13:36:15 +0530

    program

---
+++ linked_list_programs/insertBegin.py
@@ -0,0 +1,15 @@
+class Node:
+    def __init__(self,key):
+        self.key = key
+        self.next = None
+
+def insertBegin(head,key):
+    temp = Node(key)
+    temp.next = head
+    return temp
+
+#driver code
+head = None
+head = insertBegin(head,10)
+head = insertBegin(head,20)
+head = insertBegin(head,30)
